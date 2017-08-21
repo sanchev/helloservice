9f3c822cbaedbedec09b4988a2d603c3a4e61fd4
Alex <sanchev_temp@inbox.ru>
2017-08-22 00:03:36 +0300

    Implemented ContactService

--- src/main/java/com/sanchev/servlets/ContactServiceImpl.java
+++ src/main/java/com/sanchev/servlets/ContactServiceImpl.java
@@ -2,11 +2,26 @@ package com.sanchev.servlets;
 
 import com.sanchev.Contact;
 import com.sanchev.base.ContactService;
+import com.sanchev.base.DBService;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.regex.Pattern;
 
 public class ContactServiceImpl implements ContactService {
-    public Collection<Contact> getUsers(String regex) {
-        return null;
+    private final DBService dbService;
+
+    public ContactServiceImpl(DBService dbService) {
+        this.dbService = dbService;
+    }
+
+    public Collection<Contact> getFilteredContacts(String regex) {
+        Collection<Contact> contacts = new ArrayList<Contact>();
+        Pattern pattern = Pattern.compile(regex);
+        for (Contact contact : dbService.getAllContacts()) {
+            if (!pattern.matcher(contact.getName()).matches())
+                contacts.add(contact);
+        }
+        return contacts;
     }
 }

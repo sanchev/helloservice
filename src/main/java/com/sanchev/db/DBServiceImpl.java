4471ca49382dc074b77a097ca7d0f092c524faec
Alex <sanchev_temp@inbox.ru>
2017-08-22 00:20:47 +0300

    Implemented dummy DBService

---
+++ src/main/java/com/sanchev/db/DBServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sanchev.db;
+
+import com.sanchev.Contact;
+import com.sanchev.base.DBService;
+
+import java.util.Collection;
+
+public class DBServiceImpl implements DBService {
+    public Collection<Contact> getAllContacts() {
+        return null;
+    }
+}

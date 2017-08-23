8954ebaa3149e2dbc0063c365f8650d5cc2d9dcb
Alex <sanchev_temp@inbox.ru>
2017-08-23 21:42:21 +0300

    Added tests for DBServiceImpl

---
+++ src/test/java/db/DBServiceImplTest.java
@@ -0,0 +1,31 @@
+package db;
+
+import com.sanchev.base.Contact;
+import com.sanchev.db.DBServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+public class DBServiceImplTest {
+
+    private static final Logger LOGGER = LogManager.getLogger(DBServiceImplTest.class.getName());
+
+    @Test
+    public void testGetAllContacts() {
+        LOGGER.info("testGetAllContacts()");
+
+        DBServiceImpl dbService = new DBServiceImpl(DBServiceImpl.DBType.H2, "test", "test", "./h2db_test");
+        Collection<Contact> contacts = dbService.getAllContacts();
+        LOGGER.info(String.format("All contacts from db: %s", contacts));
+
+        DBServiceDummy dbServiceDummy = new DBServiceDummy();
+        Collection<Contact> expectedContacts = dbServiceDummy.getAllContacts();
+        LOGGER.info(String.format("Expected all contacts: %s", contacts));
+
+        assertEquals(contacts, expectedContacts);
+    }
+}
